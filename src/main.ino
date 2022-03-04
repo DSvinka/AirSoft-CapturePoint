828eabfc8b605269099c920fad4a08943b38d6d8
Дон <dsvinkabot@gmail.com>
2022-03-04 16:48:15 +0300

    Исправлен баг с кнопками

--- src/main.ino
+++ src/main.ino
@@ -1,25 +1,30 @@
+#include "GyverButton.h"
 #include "GyverTM1637.h"
 #include "TimerMs.h"
 #include <Arduino.h>
 
 #pragma region Настройка
 
-// Левый Дисплей
+// Пины Левого Дисплея
 #define LEFT_DISPLAY_CLK 3
 #define LEFT_DISPLAY_DIO 4
 
-// Правый Дисплей
+// Пины Правого Дисплея
 #define RIGHT_DISPLAY_CLK 5
 #define RIGHT_DISPLAY_DIO 6
 
-// Кнопки
+// Пины Кнопки
 #define LEFT_TEAM_BUTTON_PIN 7
 #define RIGHT_TEAM_BUTTON_PIN 8
 
-// Пищалка
-#define SOUND_PIN 9
+// Пины Лампочки
+#define LEFT_TEAM_LED_PIN 9
+#define RIGHT_TEAM_LED_PIN 10
 
-// Раз в сколько секунд будут выдаваться очки.
+// Пины Пищалки
+#define SOUND_PIN 11
+
+// Раз во сколько секунд будут выдаваться очки.
 const int SECONDS_TO_ADD_SCORE = 5;
 
 // Сколько секунд нужно держать кнопку чтоб захватить точку.
@@ -28,10 +33,10 @@ const int SECONDS_ON_BUTTON_TO_CHANGE_TEAM = 5;
 // Сколько очков будет выдаваться.
 const int ADD_SCORE_COUNT = 1;
 
-// Яркость дисплея команды которой начисляют очки. (От 0 до 7)
+// Яркость дисплея команды которая захватила точку. (От 0 до 7)
 const int CURRENT_TEAM_DISPLAY_BRIGHTNRSS = 7;
-// Яркость дисплея команды которой не начисляют очки. (От 0 до 7)
-const int OTHER_TEAM_DISPLAY_BRIGHTNRSS = 5;
+// Яркость дисплея команды которая не захватила точку. (От 0 до 7)
+const int OTHER_TEAM_DISPLAY_BRIGHTNRSS = 4;
 
 #pragma endregion
 
@@ -57,14 +62,14 @@ enum SoundType {
 GyverTM1637 leftDisplay(LEFT_DISPLAY_CLK, LEFT_DISPLAY_DIO);
 GyverTM1637 rightDisplay(RIGHT_DISPLAY_CLK, RIGHT_DISPLAY_DIO);
 
+GButton leftButton(LEFT_TEAM_BUTTON_PIN);
+GButton rightButton(RIGHT_TEAM_BUTTON_PIN);
+
 TimerMs addScoreTimer(SECONDS_TO_ADD_SCORE * 1000, 0, 0);
 TimerMs changeTeamButtonTimer(SECONDS_ON_BUTTON_TO_CHANGE_TEAM * 1000, 0, 0);
 
 int leftScore = 0;
 int rightScore = 0;
-
-int leftButtonState = 0;
-int rightButtonState = 0;
 
 TeamType currentTeam = None;
 int currentScore = 0;
@@ -76,13 +81,18 @@ int currentScore = 0;
 void setup() {
 	Serial.begin(9600);
 
-	setupButtons();
+	setupButton(leftButton);
+	setupButton(rightButton);
+
 	setupSoundSignal();
+	setupLights();
+
 	setupDisplay(leftDisplay);
 	setupDisplay(rightDisplay);
 	setupTimers();
 
 	serialLog("==== Started ====");
+	playSound(Multiple);
 }
 
 void setupTimers() {
@@ -92,10 +102,20 @@ void setupTimers() {
 	changeTeamButtonTimer.stop();
 }
 
-void setupButtons() {
-	serialLog("Setup Buttons...");
-	pinMode(LEFT_TEAM_BUTTON_PIN, INPUT);
-	pinMode(RIGHT_TEAM_BUTTON_PIN, INPUT);
+void setupLights() {
+	serialLog("Setup Lights...");
+
+	pinMode(LEFT_TEAM_LED_PIN, OUTPUT);
+	pinMode(RIGHT_TEAM_LED_PIN, OUTPUT);
+}
+
+void setupButton(GButton button) {
+	serialLog("Setup Button...");
+	button.setDebounce(50);        // настройка антидребезга (по умолчанию 80 мс)
+	button.setTimeout(500);        // настройка таймаута на удержание (по умолчанию 500 мс)
+	button.setClickTimeout(600);   // настройка таймаута между кликами (по умолчанию 300 мс)
+	button.setType(HIGH_PULL);
+	button.setDirection(NORM_OPEN);
 }
 
 void setupSoundSignal() {
@@ -106,6 +126,7 @@ void setupSoundSignal() {
 void setupDisplay(GyverTM1637 display) {
 	serialLog("Setup Display...");
 	display.clear();
+	display.displayInt(0);
 	display.brightness(OTHER_TEAM_DISPLAY_BRIGHTNRSS);
 }
 
@@ -114,7 +135,9 @@ void setupDisplay(GyverTM1637 display) {
 #pragma region Функционал
 
 void loop() {
-	updateButtons();
+	leftButton.tick();
+	rightButton.tick();
+
 	TeamType buttonTeam = checkCurrentTeam();
 	if (currentTeam == buttonTeam && changeTeamButtonTimer.active()) {
 		changeTeamButtonTimer.stop();
@@ -132,16 +155,11 @@ void loop() {
 	}
 }
 
-void updateButtons() {
-	leftButtonState = digitalRead(LEFT_TEAM_BUTTON_PIN);
-	rightButtonState = digitalRead(LEFT_TEAM_BUTTON_PIN);
-}
-
 TeamType checkCurrentTeam() {
-	if (leftButtonState == HIGH) {
+	if (leftButton.isHold()) {
 		return LeftTeam;
 	}
-	else if (rightButtonState == HIGH) {
+	else if (rightButton.isHold()) {
 		return RightTeam;
 	}
 	return currentTeam;
@@ -184,6 +202,25 @@ void updateDisplay(TeamType teamType, int score) {
 	}
 }
 
+void updateLed(TeamType teamType) {
+	switch (teamType) {
+		case LeftTeam:
+			digitalWrite(LEFT_TEAM_LED_PIN, HIGH);
+			digitalWrite(RIGHT_TEAM_LED_PIN, LOW);
+			break;
+
+		case RightTeam:
+			digitalWrite(LEFT_TEAM_LED_PIN, LOW);
+			digitalWrite(RIGHT_TEAM_LED_PIN, HIGH);
+			break;
+
+		default:
+			serialLog("UpdateLed ERROR: Team " + String(teamType) + " Not Provider");
+			playSound(Error);
+	}
+}
+
+
 #pragma endregion
 
 #pragma region Callback для таймеров
@@ -195,13 +232,13 @@ void scoreTimerCallback() {
 
 	currentScore = addScore(currentTeam);
 	updateDisplay(currentTeam, currentScore);
+	updateLed(currentTeam);
 	playSound(Single);
 
 	serialLog("Score Added To Team " + String(currentTeam) + ": Total Score = " + String(currentScore));
 }
 
 void changeTeamButtonTimerCallBack() {
-	updateButtons();
 	TeamType buttonTeam = checkCurrentTeam();
 	if (currentTeam == buttonTeam) {
 		return;
